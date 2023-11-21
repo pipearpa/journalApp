9fdcbd092f378ecf662a70ff6327501cefc78726
pipearpa <pipe.arpa07@gmail.com>
2023-11-20 20:35:09 -0500

    Se implementa la parte logia del login y el register

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,78 +1,93 @@
-import {Link as RouterLink} from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import { Google } from '@mui/icons-material'
 import { AuthLayout } from '../layout/AuthLayout'
+import { useState, useEffect } from "react";
+
+
 
 export const LoginPage = () => {
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const ValidarUsuario = async (nombre, password) => {
+    let response = await fetch(`http://localhost:3006/api/v1/users/${nombre}/${password}`, {
+      method: "POST",
+    });
+    if (response.status === 200) {
+      alert("inicie sesion");
+
+    } else {
+      alert("usuario o contraseña incorrecta");
+      /*  <Router>
+            
+              <Routes>
+              <Route path="/" component={AppUser} />
+              </Routes>
+      </Router>*/
+    }
+  };
+
+  const controladorDelEnvio = (e) => {
+    e.preventDefault();
+    ValidarUsuario(name, password);
+  };
+
   return (
     <AuthLayout title='Bienvenido a LookStyle'>
-      <form>
-          <Grid container>
-            <Grid item xs={12} sx={{ mt: 2 }}>
-              <TextField
-                label="Correo"
-                type='email'
-                placeholder='correo@gmail.com'
+      <form onSubmit={controladorDelEnvio} >
+      <Grid container>
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Nombre" 
+                type="text" 
+                placeholder="Ingresa Usuario"
                 fullWidth
+                name="name"
+                value={ name }
+                onChange={(e) => setName(e.target.value)}
               />
             </Grid>
 
-            <Grid item xs={12} sx={{ mt: 2 }}>
-              <TextField
-                label="Contraseña"
-                type="password"
-                placeholder='Contraseña'
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Contraseña" 
+                type="password" 
+                placeholder="Ingresa Contraseña"
                 fullWidth
+                name="password"
+                value={ password }
+                onChange={(e) => setPassword(e.target.value)}
               />
             </Grid>
 
-            <Grid container spacing={2} sx={{ mb: 2 }} mt={2}>
-
-              <Grid item xs={12} >
-                <Button variant='contained' fullWidth>
+            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <Button
+                  type="submit" 
+                  variant='contained' 
+                  fullWidth>
                   Iniciar Sesion
                 </Button>
-
               </Grid>
-              <Grid item xs={12} >
-                <Button variant='contained' fullWidth>
-                  <Google />
-                  <Typography sx={{ ml: 1 }}> Google</Typography>
-                </Button>
-
-              </Grid>
-
-
-            </Grid>
-
-
+           </Grid>
             
-
-            
-
-            
-            
-            
-            <Grid container direction='row' justifyContent='end'>
-            <Typography sx={{ mr: 1}}>
-              ¿No tienes cuenta?
-            </Typography>
-            <Link component={RouterLink} color='inherit' to="/auth/register">
-              Crear una cuenta 
-              </Link>
-
-              </Grid>
+        </Grid>
+      </form>
 
 
-          </Grid>
 
+      <Link component={RouterLink} color='inherit' to="/auth/register">
+        Crear una cuenta
+      </Link>
 
-        </form>
 
     </AuthLayout>
 
-        
 
-      
+
+
   )
 }
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,15 +1,16 @@
-import { Link as RouterLink } from 'react-router-dom'
-import { Button, Grid, Link, TextField, Typography } from '@mui/material'
-import { Google } from '@mui/icons-material'
-import { AuthLayout } from '../layout/AuthLayout'
+// Importaciones de bibliotecas y componentes necesarios
+import { Link as RouterLink } from 'react-router-dom'; // Importa el componente Link de react-router-dom con el nombre RouterLink
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'; // Importa componentes de Material-UI
+import { AuthLayout } from '../layout/AuthLayout'; // Importa un layout de autenticación desde un directorio llamado 'layout'
 
+// Definición del componente RegisterPage como una función de flecha
 export const RegisterPage = () => {
   return (
+    // Usa el componente AuthLayout con un título específico para la página de registro
     <AuthLayout title='Crear una cuenta'>
       <form>
-
         <Grid container>
-
+          {/* Primer campo de entrada: Nombre Completo */}
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               label="Nombre Completo"
@@ -19,7 +20,7 @@ export const RegisterPage = () => {
             />
           </Grid>
 
-
+          {/* Segundo campo de entrada: Correo */}
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               label="Correo"
@@ -29,6 +30,7 @@ export const RegisterPage = () => {
             />
           </Grid>
 
+          {/* Tercer campo de entrada: Contraseña */}
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               label="Contraseña"
@@ -38,37 +40,27 @@ export const RegisterPage = () => {
             />
           </Grid>
 
+          {/* Botón para crear cuenta */}
           <Grid container spacing={2} sx={{ mb: 2 }} mt={2}>
-
-            <Grid item xs={12} >
+            <Grid item xs={12}>
               <Button variant='contained' fullWidth>
                 Crear Cuenta
               </Button>
-
             </Grid>
           </Grid>
 
-          <Grid container direction='row' justifyContent='end' >
-            <Typography sx={{ mr: 1}}>
+          {/* Enlace para iniciar sesión */}
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr: 1 }}>
               ¿Ya tienes cuenta?
             </Typography>
+            {/* Enlace usando RouterLink para redirigir a la página de inicio de sesión */}
             <Link component={RouterLink} color='inherit' to="/auth/login">
               Ingresar
             </Link>
-
-
           </Grid>
-
-
         </Grid>
-
-
       </form>
-
     </AuthLayout>
-
-
-
-
-  )
-}
+  );
+};
